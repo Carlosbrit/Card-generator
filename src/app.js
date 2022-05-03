a648f8644005980f70ee007380d659550fada12c
Carlosbrit <cb007842@gmail.com>
2022-05-03 19:27:26 +0000

    qer

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").innerHTML = genrateRandomSuite();
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+};
+
+let generateRandomNumber = () => {
+  let values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let indexValues = Math.floor(Math.random() * values.length);
+
+  return values[indexValues];
+};
+let genrateRandomSuite = () => {
+  let suite = ["diamond", "spade", "heart", "club"];
+  let indexSuites = Math.floor(Math.random() * suite.length);
+  return suite[indexSuites];
 };
